03444009b5cc276edf7b7f93d37d3ffceb40365c
Web Server <spyman40@gmail.com>
2012-09-27 13:12:09 +0000

    updated the stylesheets a bit, added a side bar

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Chris\'s Site' });
 };
+
+exports.about = function(req, res){
+  res.render('about', {title: 'About Chris'});
+};
